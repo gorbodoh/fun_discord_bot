fc0528bf4ca50d8db0676df16592c17e8c6b3967
Gorbodoh <102156984+gorbodoh@users.noreply.github.com>
2024-12-23 23:57:13 +0800

    Initial commit.

---
+++ fun_discord_bot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        String token = File.ReadAllText(Directory.GetCurrentDirectory() + "\\token.txt");
+        Console.WriteLine(token);
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
